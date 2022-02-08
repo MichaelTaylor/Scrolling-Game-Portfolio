1ccd555ca9dc356d666cd2459dc16d7f2abdf49e
Michael Taylor <michael.taylor.gamedev@gmail.com>
2022-02-08 06:13:56 -0600

    No more resume

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Styles from "./About.module.css";
 import Avatar from "../images/PD Yearbook Portraite.svg";
 import EmailLogo from "../images/Email Logo.svg";
-import PDFFile from "../components/PDFFile";
 import ContactForm from "../components/ContactForm";
 
 const About = () => {
